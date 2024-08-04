2fe16c15f270c7520f0ab4c96e39fe2f3f2b5323
Rhys <rhys.murage@apainsurance.org>
2024-08-04 14:24:27 +0300

    Add more problems and solutions

--- MinChange.java
+++ MinChange.java
@@ -4,8 +4,18 @@ import java.util.HashMap;
 import java.lang.Integer;
 
 class MinChange {
+    /**
+     * Write a function minChange that takes in an amount and an array of coins. 
+     * The function should return the minimum number of coins required to create the amount. 
+     * You may use each coin as many times as necessary.
+     * If it is not possible to create the amount, then return -1.
+     */
     public static void main(String[] args) {
-        minChange(4, List.of(1, 2, 3));
+        System.out.println(minChange(4, List.of(1, 2, 3)));
+        System.out.println(minChange(23, List.of(2,5,7))); //4
+        System.out.println(minChange(271, List.of(10, 8, 265, 24))); // -> -1);
+        System.out.println(minChange(102, List.of(1, 5, 10, 25))); // -> 6
+        System.out.println(minChange(8, List.of(1,5,4,12))); // --> 2, because 4+4 is the minimum coins possible
     }
 
     public static int minChange(int amount, List<Integer> coins) {
